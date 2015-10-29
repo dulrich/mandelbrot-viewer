24050df9bd5d6d163d678c6656f2e48b20145ccc
David Ulrich <dulrich@atomicinfotech.com>
2015-10-29 13:54:28 -0700

    add interactive zooming and shifting with curses

--- main.c
+++ main.c
@@ -1,34 +1,46 @@
+// mandelbrot viewer: curses mandelbrot explorer
+// Copyright (C) 2015  David Ulrich
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <ncurses.h>
 #include <stdio.h>
 
-int main() {
-	int k;
-	
-	// STUFF TO CHANGE
-	
-	int zoom = 1;
-	
-	int x_shift = -11;
-	int y_shift = 0;
-	
-	// if you have a larger terminal...
-	int width = 50;
-	int height = 20;
-	
-	// END STUFF TO CHANGE
-	
+int render(int zoom,int shift_x,int shift_y,int width,int height) {
 	int x_scale = 25 * zoom;
 	int y_scale = 10 * zoom;
 	
-	x_shift = (x_shift * zoom) + width * (zoom - 1);
-	y_shift = y_shift * zoom;
+	shift_x = (shift_x * zoom) + width * (zoom - 1);
+	shift_y = shift_y * zoom;
 	
-	float i,j,r,x,y=-height+y_shift;
+	float i,j,r;
+	int k;
 	
-	while(puts(""),y++ < (height - 1 + y_shift)) {
+	float x,y;
+	y = -height + shift_y;
+	
+	erase();
+	
+	int c = 0;
+	while(c++,y++ < (height - 1 + shift_y)) {
+		move(c,0);
+		refresh();
+		
 		for(
-			x = 0 + x_shift;
-			x++ < 2 * width + x_shift;
-			putchar(" .:-;!/>)|&IH%*#"[k&15])
+			x = 0 + shift_x;
+			x++ < 2 * width + shift_x;
+			addch(" .:-;!/>)|&IH%*#"[k&15])
 		) {
 			for(
 				i=k=r=0;
@@ -37,6 +49,63 @@ int main() {
 			);
 		}
 	}
+	
+	return 0;
+}
+
+int main() {
+	int zoom = 1;
+	
+	int shift_x = -11;
+	int shift_y = 0;
+	
+	// if you have a larger terminal...
+	// should get curses window size
+	int width = 50;
+	int height = 20;
+	
+	int c = 0;
+	
+	initscr();
+	
+	noecho();
+	cbreak();
+	keypad(stdscr,TRUE);
+	
+	do {
+		if (c == 'x' || c == 'X' || c == 'q' || c == 'Q') {
+			break;
+		}
+		else if (c == '+') {
+			zoom++;
+		}
+		else if (c == '-') {
+			zoom--;
+		}
+		else if (c == KEY_UP) {
+			shift_y--;
+		}
+		else if (c == KEY_DOWN) {
+			shift_y++;
+		}
+		else if (c == KEY_LEFT) {
+			shift_x--;
+		}
+		else if (c == KEY_RIGHT) {
+			shift_x++;
+		}
+		
+		getmaxyx(stdscr,height,width);
+		
+		if (zoom < 1) zoom = 1;
+		
+		render(zoom,shift_x,shift_y,width/2,height/2);
+		
+		refresh();
+		c = getch();
+	} while(c);
+	
+	endwin();
 	
 	return 0;
 }
