a3f2762978e074b0afaf6f03690e1d35d82fa22b
David Ulrich <dulrich@atomicinfotech.com>
2015-10-30 09:48:12 -0700

    something about color support is broken

--- main.c
+++ main.c
@@ -17,14 +17,15 @@
 #include <ncurses.h>
 #include <stdio.h>
 
-int render(float zoom,float shift_x,float shift_y,int width,int height) {
+int render(short color,float zoom,float shift_x,float shift_y,int width,int height) {
 	int x_scale = 25 * zoom;
 	int y_scale = 10 * zoom;
 	
 	shift_x = shift_x + width * (zoom - 1);
 	
 	float i,j,r;
-	int k;
+	int k,kk;
+	kk = 0;
 	
 	float x,y;
 	y = -height + shift_y;
@@ -38,13 +39,18 @@ int render(float zoom,float shift_x,float shift_y,int width,int height) {
 		for(
 			x = 0 + shift_x;
 			x++ < 2 * width + shift_x;
-			addch(" .:-;!/>)|&IH%*#"[k&15])
 		) {
 			for(
 				i=k=r=0;
 				j=r*r-i*i-2+x/x_scale,i=2*r*i+y/y_scale,j*j+i*i<11&&k++<111
 				;r=j
 			);
+			if (color && (k&15) != kk) {
+				attroff(COLOR_PAIR(k&15));
+				attron(COLOR_PAIR(k&15));
+				kk = k&15;
+			}
+			addch(" .:-;!/>)|&IH%*#"[k&15]);
 		}
 	}
 	
@@ -64,12 +70,54 @@ int main() {
 	int height = 20;
 	
 	int c = 0;
+	short color;
 	
 	initscr();
 	
 	noecho();
 	cbreak();
 	keypad(stdscr,TRUE);
+	
+	
+	color = COLORS >= 256;
+	
+	if (color) {
+		start_color();
+		
+		init_color(0,62*0,0,0);
+		init_color(1,62*1,0,0);
+		init_color(2,62*2,0,0);
+		init_color(3,62*3,0,0);
+		init_color(4,62*4,0,0);
+		init_color(5,62*5,0,0);
+		init_color(6,62*6,0,0);
+		init_color(7,62*7,0,0);
+		init_color(8,62*8,0,0);
+		init_color(9,62*9,0,0);
+		init_color(10,62*10,0,0);
+		init_color(11,62*11,0,0);
+		init_color(12,62*12,0,0);
+		init_color(13,62*13,0,0);
+		init_color(14,62*14,0,0);
+		init_color(15,62*15,0,0);
+		
+		init_pair(0,0,0);
+		init_pair(1,1,0);
+		init_pair(2,2,0);
+		init_pair(3,3,0);
+		init_pair(4,4,0);
+		init_pair(5,5,0);
+		init_pair(6,6,0);
+		init_pair(7,7,0);
+		init_pair(8,8,0);
+		init_pair(9,9,0);
+		init_pair(10,10,0);
+		init_pair(11,11,0);
+		init_pair(12,12,0);
+		init_pair(13,13,0);
+		init_pair(14,14,0);
+		init_pair(15,15,0);
+	}
 	
 	do {
 		if (c == 'x' || c == 'X' || c == 'q' || c == 'Q') {
@@ -104,7 +152,7 @@ int main() {
 		
 		if (zoom < 1) zoom = 1;
 		
-		render(zoom,shift_x,shift_y,width/2,height/2);
+		render(color,zoom,shift_x,shift_y,width/2,height/2);
 		
 		refresh();
 		
